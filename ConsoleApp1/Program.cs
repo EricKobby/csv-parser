827a6980ccf29d83fe110b4418bf276bbc75c1ac
Eric Kobby Mensah <mensaheric743@hotmail.com>
2021-02-24 00:08:07 +0000

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿using Csv.Parser;
+using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var parser = new CsvParser("test.csv");
+
+            var departments = parser.Deserialize<department>();
+            foreach (var dept in departments)
+            {
+                Console.WriteLine(dept.departmentName);
+            }
+            Console.ReadKey();
+        }
+    }
+
+    class department
+    {
+        public int departmentId { get; set; }
+        public string departmentName { get; set; }
+        public int headEmployeeId { get; set; }
+        public string departmentCode { get; set; }
+        public int divisionId { get; set; }
+    }
+}
---
+++ Csv.Parser/CsvParser.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Csv.Parser
+{
+    public class CsvParser
+    {
+
+        private readonly string _csvPath;
+
+        public CsvParser(string csvPath) => _csvPath = csvPath;
+
+        public IEnumerable<T> Deserialize<T>()
+        {
+            var rawData = File.ReadLines(_csvPath).ToList();
+            var columns = rawData.First().Split(',');
+            var rawRows = rawData.Skip(1).ToList();
+            var listOfItems = new List<T>();
+
+
+            for (int j = 0; j < rawRows.Count; j++)
+            {
+                var currentRow = rawRows[j].Split(',');
+                var instance = Activator.CreateInstance<T>();
+
+                for (int k = 0; k < currentRow.Length; k++)
+                {
+                    var currentProperty = instance.GetType().GetProperty(columns[k]);
+                    
+                    if (currentProperty is null)continue;
+
+                    var currentPropertyValue = currentRow[k].Trim('"');
+
+                    if (!string.IsNullOrEmpty(currentPropertyValue))
+                        currentProperty.SetValue(instance, Convert.ChangeType(currentPropertyValue, currentProperty.PropertyType));
+                }
+                listOfItems.Add(instance);
+            }
+
+            return listOfItems;
+        }
+    }
+}
